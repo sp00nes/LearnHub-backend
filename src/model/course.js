52326952aa3da9c1f6678c894ae443be15fe2f6e
MARTIN HANSEN <marrtt1234@gmail.com>
2023-04-20 18:16:13 +0000

    course

--- src/model/course.js
+++ src/model/course.js
@@ -8,14 +8,8 @@ const courseSchema = new Schema({
   courseName: { type: String, required: true },
   owner_id: { type: String, required: true },
   description: { type: String, required: true },
-  modules: [{ 
-    name: String, 
-    idx: Number,  
-    lessonText: String,
-    Img: String,
-    questions: [{questionTxt: String, answer: String, answerArr: {type: Array, required: true}}],
-  }],
-  activeCourses: [{ course_id: String, complete: Boolean }],
+  img_Url: { type: String },
+  modules: { type: Array },
 });
 
 const courseModel = mongoose.model('course', courseSchema);
